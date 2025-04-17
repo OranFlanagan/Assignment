3319daee52e4b8974464207c37c656bca5411c6e
Oran Flanagan <G00428429@atu.ie>
2025-04-17 12:33:35 +0100

    Beginning to understand what is being asked

--- Assignment.c
+++ Assignment.c
@@ -36,17 +36,54 @@ typedef struct
 } LoginT;
 
 MachineryT head = NULL;
-
-void login();
-void displayMenu();
-void addMachine();
-void displayAllMachines();
-void displayMachineByChassis();
-void clearBuffer();
-int validateEmail(char* email);
-void printPassword(char* pass);
-
-void main()
+void printPassword(char* pass)
 {
-	
+    int i = 0;
+    char ch;
+    while (i < password_Legth)
+    {
+        ch = _getch();
+        if (ch == '\r') break;
+        pass[i++] = ch;
+        printf("*");
+    }
+    pass[i] = '\0';
+}
+
+void login()
+{
+    LoginT logins[3] = {
+        {"admin1", "pass01"},
+        {"admin2", "pass02"},
+        {"admin3", "pass03"}
+    };
+
+    char enteredUser[20];
+    char enteredPass[password_Legth + 1];
+    int authenticated = 0;
+
+    printf("Enter username: ");
+    scanf("%s", enteredUser);
+    printf("Enter password: ");
+    printPassword(enteredPass);
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (strcmp(enteredUser, logins[i].userName) == 0 &&
+            strcmp(enteredPass, logins[i].passWord) == 0)
+        {
+            authenticated = 1;
+            break;
+        }
+    }
+
+    if (!authenticated)
+    {
+        printf("\nAccess denied.\n");
+        exit(0);
+    }
+    else
+    {
+        printf("\nLogin successful!\n");
+    }
 }
